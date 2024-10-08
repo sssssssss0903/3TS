51476edb1feddf2a9c4f29fda74683c25725bcb8
Echo_o <2662810357@qq.com>
2024-10-08 20:22:52 +0800

    Update auto_test_all.sh

--- src/dbtest/auto_test_all.sh
+++ src/dbtest/auto_test_all.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 set -x
-./auto_test.sh "mysql" "read-uncommitted"
-./auto_test.sh "mysql" "read-committed"
-./auto_test.sh "mysql" "repeatable-read"
-./auto_test.sh "mysql" "serializable"
-
-./auto_test.sh "sqlserver" "read-uncommitted"
-./auto_test.sh "sqlserver" "read-committed"
-./auto_test.sh "sqlserver" "repeatable-read"
-./auto_test.sh "sqlserver" "serializable"
+./auto_test.sh "pg" "read-uncommitted"
 
 ./auto_test.sh "firebird" "read-committed"
